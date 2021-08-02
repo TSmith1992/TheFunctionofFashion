a1823b8570933ee13e1fcc330a893e62cd22c64c
AndresCD-dev <86489537+AndresCD-dev@users.noreply.github.com>
2021-08-02 14:50:54 -0400

    Added buy button and comment with filter prototype.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-function init(){
+function init() {
     productRender()
 }
 
-//func to iterate over product list
-function productRender(){
-    productList.forEach(product => showProducts(product))
-}
+// fetch("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-seller-products?domainCode=com&sellerId=AD97MR4NOW5CD&page=1", {
+//     "method": "GET",
+//     "headers": {
+//         "x-rapidapi-key": "84e9bccac5msh0b2a729d7675d15p134f8djsne7d94130722a",
+//         "x-rapidapi-host": "axesso-axesso-amazon-data-service-v1.p.rapidapi.com"
+//     }
+// })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
 
-//func to create product card
-function showProducts(product){
-    const pContainer = document.getElementById('products-container');
+
+function productRender() {
+    productList.forEach(product => showProducts(product))
+    console.log("hello")
+}
+function showProducts(product) {
+    const pContainer = document.getElementById('products-container')
     const productCard = document.createElement('div');
     const lineBreak = document.createElement('hr')
     const productImg = document.createElement('img');
@@ -18,31 +31,42 @@ function showProducts(product){
     const productPrime = document.createElement('p');
     const productDescript = document.createElement('span');
     const productBuyButton = document.createElement('button');
-    const buttonBreak = document.createElement('br')
 
 
-    productCard.setAttribute('id',`product ${product.asin}`);
-    productCard.setAttribute('class','product-card')
-    productImg.src=`${product.imgUrl}`;
-    productRating.innerText=`${product.productRating}`;
-    productPrice.innerText=`Price: $${product.price}`;
-    productPrime.innerText ='Is this product exclusive for PRIME Members? ' + forPrimeMembers();
-    productDescript.innerText = `${product.productDescription}.`;
+    productCard.setAttribute('id', `product ${product.asin}`);
+    productCard.setAttribute('class', 'product-card')
+    productImg.setAttribute("class", "image")
+    productImg.src = `${product.imgUrl}`;
+    productRating.innerText = `${product.productRating}`;
+    productPrice.innerText = `Price: $${product.price}`;
+    productPrime.innerText = 'Is this product exclusive for PRIME Members? '
+    productDescript.innerText = product.productDescription;
     productBuyButton.innerText = 'Buy';
 
-    productCard.append(productImg,productRating,productPrice,productPrime,productDescript,buttonBreak,productBuyButton,lineBreak)
+    productCard.append(productImg, productRating, productPrice, productPrime, productDescript, productBuyButton, lineBreak)
     pContainer.appendChild(productCard)
+
+
+
+
+    productBuyButton.addEventListener("click", function (e) {
+        const shoppingCart = document.querySelector("#shopping-bag")
+        const newCard = e.target.productCard
+        shoppingCart.append(newCard)
+
+
+
+    })
+
+
+    // const dropDown = document.getElementsByName("clothingprice")
+    // dropDown.addEventListener("click", function () {
+    //     const options = dropDown.querySelectorAll("option")
+    //     if (options === "High") {
+
+    //     }
+    // })
 }
 
-//func to tell if a product is exclusively for Prime Members
-function forPrimeMembers(){
-    if (productList.prime){
-        return 'Yes'
-    }else{
-        return 'No'
-    }
-}
 
 init()
-
-//ctrl shift n
