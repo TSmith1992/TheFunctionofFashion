d93c91fd08ffd1456c682cd7b0ec37a4eb9f7c3d
Tarique Smith <tpsmith1992@gmail.com>
2021-08-02 15:30:03 -0400

    Switched dropdown default choices

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function init() {
     productRender()
+    PrimeExclusive()
 }
 
 // fetch("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-seller-products?domainCode=com&sellerId=AD97MR4NOW5CD&page=1", {
@@ -22,16 +23,6 @@ function productRender() {
     console.log("hello")
 }
 function showProducts(product) {
-    const pContainer = document.getElementById('products-container')
-    const productCard = document.createElement('div');
-    const lineBreak = document.createElement('hr')
-    const productImg = document.createElement('img');
-    const productRating = document.createElement('p');
-    const productPrice = document.createElement('p');
-    const productPrime = document.createElement('p');
-    const productDescript = document.createElement('span');
-    const productBuyButton = document.createElement('button');
-
 
     productCard.setAttribute('id', `product ${product.asin}`);
     productCard.setAttribute('class', 'product-card')
@@ -39,13 +30,12 @@ function showProducts(product) {
     productImg.src = `${product.imgUrl}`;
     productRating.innerText = `${product.productRating}`;
     productPrice.innerText = `Price: $${product.price}`;
-    productPrime.innerText = 'Is this product exclusive for PRIME Members? '
+    productPrime.innerText = 'Is this product exclusive for PRIME Members? '+ primeCheck();
     productDescript.innerText = product.productDescription;
     productBuyButton.innerText = 'Buy';
 
     productCard.append(productImg, productRating, productPrice, productPrime, productDescript, productBuyButton, lineBreak)
     pContainer.appendChild(productCard)
-
 
 
 
@@ -57,8 +47,13 @@ function showProducts(product) {
 
 
     })
-
-
+    function primeCheck(){
+        if (product.prime){
+            return 'Yes'
+        } else {
+            return 'No'
+        }
+    }
     // const dropDown = document.getElementsByName("clothingprice")
     // dropDown.addEventListener("click", function () {
     //     const options = dropDown.querySelectorAll("option")
@@ -67,6 +62,18 @@ function showProducts(product) {
     //     }
     // })
 }
+
+function PrimeExclusive(){
+    const primeBtn = document.querySelector('[name="forPrime"]')
+    
+    primeBtn.addEventListener('click', e =>{
+        if (e.target.value ='Yes'){
+        
+        }
+    })
+}
+
+
 
 
 init()
