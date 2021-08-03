6994520bbd4926d2549fa1d83f7d76cad4e32e99
AndresCD-dev <86489537+AndresCD-dev@users.noreply.github.com>
2021-08-03 15:17:07 -0400

    Added remove and buy buttons to work and add total to cart.
    Adding the 2 buttons for the shopping cart, works but the total is not updated to the new one as of yet.

--- index.js
+++ index.js
@@ -3,24 +3,22 @@ function init() {
 }
 
 // fetch("https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-seller-products?domainCode=com&sellerId=AD97MR4NOW5CD&page=1", {
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-key": "84e9bccac5msh0b2a729d7675d15p134f8djsne7d94130722a",
-//         "x-rapidapi-host": "axesso-axesso-amazon-data-service-v1.p.rapidapi.com"
-//     }
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "0ea4ca4479msh35a0d514694fec8p1b3d3ejsn2f6692e1d189",
+// 		"x-rapidapi-host": "axesso-axesso-amazon-data-service-v1.p.rapidapi.com"
+// 	}
 // })
-//     .then(response => {
-//         console.log(response);
-//     })
-//     .catch(err => {
-//         console.error(err);
-//     });
-
+// .then(res => res.json())
+// .then(products => console.log(products))
+// .catch(err => {
+// 	console.error(err);
+// });
 
 function productRender() {
     productList.forEach(product => showProducts(product))
-    console.log("hello")
 }
+
 function showProducts(product) {
     const pContainer = document.getElementById('products-container')
     const productCard = document.createElement('div');
@@ -31,7 +29,11 @@ function showProducts(product) {
     const productPrime = document.createElement('p');
     const productDescript = document.createElement('span');
     const productBuyButton = document.createElement('button');
-
+    const onlyPrimeProds = document.getElementById('prime-btn');
+    const allProds = document.getElementById('all-btn');
+    const lowRangebtn = document.getElementById('low-range');
+    const highRangebtn = document.getElementById('high-range');
+    const allRangebtn = document.getElementById('all-range');
 
     productCard.setAttribute('id', `product ${product.asin}`);
     productCard.setAttribute('class', 'product-card')
@@ -39,34 +41,82 @@ function showProducts(product) {
     productImg.src = `${product.imgUrl}`;
     productRating.innerText = `${product.productRating}`;
     productPrice.innerText = `Price: $${product.price}`;
-    productPrime.innerText = 'Is this product exclusive for PRIME Members? '
+    productPrime.innerText = 'Is this product exclusive for PRIME Members? ' + primeCheck();
     productDescript.innerText = product.productDescription;
     productBuyButton.innerText = 'Buy';
 
     productCard.append(productImg, productRating, productPrice, productPrime, productDescript, productBuyButton, lineBreak)
     pContainer.appendChild(productCard)
 
-
-
+    function primeCheck() {
+        if (product.prime) {
+            return 'Yes'
+        } else {
+            return 'No'
+        }
+    }
 
     productBuyButton.addEventListener("click", function (e) {
+        const priceTotal = document.querySelector("#total-price")
         const shoppingCart = document.querySelector("#shopping-bag")
-        const newCard = e.target.productCard
+        const newCard = e.target.parentElement
         shoppingCart.append(newCard)
+        productBuyButton.remove()
+        productRating.remove()
+        productDescript.remove()
+        productPrime.remove()
+        const removeButton = document.createElement("button")
+        newCard.appendChild(removeButton)
+        removeButton.innerText = "Remove"
+        showProducts(product)
+        priceTotal.innerText = `Total : ${product.price}`
 
 
 
+        removeButton.addEventListener("click", function (e) {
+            newCard.remove()
+        })
+    })
+
+    lowRangebtn.addEventListener('click', e => {
+        if (product.price > 20) {
+            productCard.style = 'display : none'
+            lowRangebtn.style.color = 'purple'
+        }
+    })
+
+    highRangebtn.addEventListener('click', e => {
+        if (product.price < 20) {
+            productCard.style = 'display : none'
+            highRangebtn.style.color = 'purple'
+        }
+    })
+
+    allRangebtn.addEventListener('click', e => {
+        productCard.style = ''
+        lowRangebtn.style.color = ''
+        highRangebtn.style.color = ''
+    })
+
+    onlyPrimeProds.addEventListener('click', e => {
+        if (productPrime.innerText == 'Is this product exclusive for PRIME Members? No') {
+            productCard.style = 'display : none'
+            onlyPrimeProds.style.color = 'purple'
+        }
+    })
+
+    allProds.addEventListener('click', e => {
+        productCard.style = ''
+        onlyPrimeProds.style.color = ''
     })
 
 
-    // const dropDown = document.getElementsByName("clothingprice")
-    // dropDown.addEventListener("click", function () {
-    //     const options = dropDown.querySelectorAll("option")
-    //     if (options === "High") {
 
-    //     }
-    // })
+
+
 }
 
 
+
 init()
+
